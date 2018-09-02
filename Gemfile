335e6b7af39d7dc51e34df61f942d79add4b4579
Sushanth BC <sushanthbc@gmail.com>
2018-09-02 12:26:56 +0530

    regex-phone-number (#6)
    * regex-phone-number

--- Gemfile
+++ Gemfile
@@ -2,23 +2,16 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.2'
-
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
-# Use Puma as the app server
 gem 'puma', '~> 3.11'
-
 gem 'bootsnap', '>= 1.1.0', require: false
 
-
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :development, :test do
   gem 'listen', '>= 3.0.5', '< 3.2'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'mysql2', '>= 0.4.4', '< 0.6.0'
   gem 'pry', '~> 0.11.3'
   gem 'pry-rails', '~> 0.3.6'
--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
-require "./app/middleware/catch_json_parse_errors"
+require "./app/middleware/catch_json_parse_errors.rb"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,7 +22,7 @@ module DozerServices
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.middleware.use CatchJsonParseErrors
+    config.middleware.insert_before Rack::Head, CatchJsonParseErrors
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
