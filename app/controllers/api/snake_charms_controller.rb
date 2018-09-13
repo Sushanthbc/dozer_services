dc6766481e83ead815fa9ee1fb0bcf56854ac297
Sushanth BC <sushanthbc@gmail.com>
2018-09-13 23:15:26 +0530

    completed-different-endpoint-admin

--- app/controllers/api/snake_charms_controller.rb
+++ app/controllers/api/snake_charms_controller.rb
@@ -2,10 +2,22 @@
 
 module Api
   class SnakeCharmsController < ApplicationController
-    def index
-      snake_charms_hash = SnakeCharm.fetch_snake_charms
-      snake_charms_processing = SnakeCharm.caudals_processing snake_charms_hash
-      render json: { snake_charm: snake_charms_processing,
+    def all_snake_charms
+      if admin_access(params[:user_id])
+        snake_charms_hash = SnakeCharm.fetch_snake_charms
+        snake_charms_processed =
+          SnakeCharm.caudals_processing snake_charms_hash
+      else
+        snake_charms_processed = 'Not Authorized to view results'
+      end
+      render json: { snake_charm: snake_charms_processed,
+                     status: 'success' }
+    end
+
+    def single_snake_charm
+      snake_charm_hash = SnakeCharm.fetch_snake_charm_user_id params[:user_id]
+      snake_charm_processed = SnakeCharm.caudals_processing snake_charm_hash
+      render json: { snake_charm: snake_charm_processed,
                      status: 'success' }
     end
 
@@ -14,6 +26,10 @@ module Api
       if user.snake_charms.create!(snake_charm_params)
         render json: { status: 'success' }
       end
+    end
+
+    def admin_access(user_id)
+      User.where(id: user_id).first.admin == '1'
     end
 
     private
@@ -28,7 +44,8 @@ module Api
                                           :snake_caudals,
                                           :snake_behavior,
                                           :snake_macro_habitat,
-                                          :snake_micro_habitat, :snake_condition,
+                                          :snake_micro_habitat,
+                                          :snake_condition,
                                           :release_date, :user_id, :snake_photo)
     end
   end
--- app/models/snake_charm.rb
+++ app/models/snake_charm.rb
@@ -17,11 +17,27 @@ class SnakeCharm < ApplicationRecord
         (caudals_string.split(';')[1]).split(':')[1]
     end
     snake_charms_hash
+  rescue Exception::NoMethodError => e
+    return snake_charms_hash = []
   end
 
   def self.fetch_snake_charms
-    snake_charm_symbolize =
-      SnakeCharm.all.map { |e| e.attributes.symbolize_keys }
-    snake_charm_symbolize
+    snake_charms = SnakeCharm.all
+    SnakeCharm.symbolize_object snake_charms
+  end
+
+  def self.fetch_snake_charm_user_id(user_id)
+    snake_charm = SnakeCharm.where(user_id: user_id)
+    symbolize_object snake_charm
+  end
+
+  def self.symbolize_object(snake_charms)
+    if snake_charms.nil?
+      snake_charms_symbolize = []
+    else
+      snake_charms_symbolize =
+        snake_charms.map { |e| e.attributes.symbolize_keys }
+    end
+    snake_charms_symbolize
   end
 end
