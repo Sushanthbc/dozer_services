95fafe8503c3bbc3f337e98ab7a4b5523b836c8d
Sushanth BC <sushanthbc@gmail.com>
2018-09-16 22:54:59 +0530

    fixed-variable-change

--- db/migrate/20180916171136_add_additional_field_user.rb
+++ db/migrate/20180916171136_add_additional_field_user.rb
@@ -3,7 +3,7 @@ class AddAdditionalFieldUser < ActiveRecord::Migration[5.2]
     add_column :users, :about_user, :string
     add_column :users, :purpose, :string
     if Rails.env.production?
-      change_column :users, :admin, 'boolean USING CAST(column_name AS boolean)',
+      change_column :users, :admin, 'boolean USING CAST(admin AS boolean)',
        default: 0
     else
      change_column :users, :admin, :boolean, default: 0
