e2d7d70caae083782f1f4489b698809716efba53
Sushanth BC <sushanthbc@gmail.com>
2018-09-16 22:51:40 +0530

    Fixed-issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_160054) do
+ActiveRecord::Schema.define(version: 2018_09_16_171136) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2018_09_16_160054) do
     t.string "last_name", null: false
     t.string "email_id", null: false
     t.string "phone", null: false
-    t.boolean "admin", default: false, null: false
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "about_user"
