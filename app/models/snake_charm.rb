92553f83449a5856498e85ff395b5b2a0cb0c4d6
Sushanth BC <sushanthbc@gmail.com>
2018-09-30 12:45:45 +0530

    fixed-issues

--- app/models/snake_charm.rb
+++ app/models/snake_charm.rb
@@ -12,13 +12,14 @@ class SnakeCharm < ApplicationRecord
   def self.caudals_processing(snake_charms_hash)
     if snake_charms_hash.length.positive?
       snake_charms_hash.each do |caudal|
-        next if caudal[:snake_caudals].nil?
-        caudals_string = caudal[:snake_caudals]
-        caudal[:snake_caudals] = {}
-        caudal[:snake_caudals][:divided] =
-          (caudals_string.split(';')[0]).split(':')[1]
-        caudal[:snake_caudals][:undivided] =
-          (caudals_string.split(';')[1]).split(':')[1]
+        unless caudal[:snake_caudals].nil?
+          caudals_string = caudal[:snake_caudals]
+          caudal[:snake_caudals] = {}
+          caudal[:snake_caudals][:divided] =
+            (caudals_string.split(';')[0]).split(':')[1]
+          caudal[:snake_caudals][:undivided] =
+            (caudals_string.split(';')[1]).split(':')[1]
+        end
         user_info = User.find_by_id(caudal[:user_id])
         created_by =
           user_info.first_name.to_s + ' ' + user_info.last_name.to_s
