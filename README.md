2aa6349f1eff61e3d2aca0a518aff477e4a1f564
Sushanth Gowda <sushanthbc@gmail.com>
2019-07-04 18:37:50 +0530

    moved from mysql to postgres (#36)

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # README
 
 * Ruby version
-```ruby-2.4.2```
-  
+```
+  ruby-2.4.2
+
+```
+
 * Database creation
 
 ```
-  - rake db:create
-  - rake db:migrate
+  rake db:create db:migrate db:seed
+
+```
+
+ ### Installation of Postgres not through Brew please run below command
+
+ ```
+ bundle config build.pg --with-pg-config=/Applications/Postgres.app/Contents/Versions/latest/bin/pg_config
+
  ```
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2018_10_08_082330) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_082330) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,7 +36,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_082330) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "snake_charms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "snake_charms", force: :cascade do |t|
     t.datetime "rescue_date_time"
     t.string "address", null: false
     t.string "village", null: false
@@ -58,7 +61,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_082330) do
     t.string "snake_caudals"
     t.decimal "latitude", precision: 10, scale: 6
     t.decimal "longitude", precision: 10, scale: 6
-    t.decimal "elevation", precision: 10
+    t.decimal "elevation"
     t.string "elevation_unit"
     t.string "general_remarks"
     t.string "bite_report"
@@ -67,7 +70,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_082330) do
     t.index ["user_id"], name: "index_snake_charms_on_user_id"
   end
 
-  create_table "snake_photo_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "snake_photo_tags", force: :cascade do |t|
     t.boolean "head_lateral_left", default: false
     t.boolean "head_lateral_right", default: false
     t.boolean "rostral", default: false
@@ -81,7 +84,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_082330) do
     t.index ["active_storage_attachment_id"], name: "index_snake_photo_tags_on_active_storage_attachment_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "email_id", null: false
